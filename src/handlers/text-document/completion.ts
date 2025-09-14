a47bbbb899868f610803615dbf04d64ae58e76d6
Krztk <52879665+Krztk@users.noreply.github.com>
2025-09-14 12:41:31 +0200

    feat: endpoint completion

--- src/handlers/text-document/completion.ts
+++ src/handlers/text-document/completion.ts
@@ -56,6 +56,28 @@ export const completion = (message: RequestMessage): CompletionList | null => {
   const currentLine = content.split("\n")[params.position.line]!;
   const lineUntilCursor = currentLine.slice(0, params.position.character);
 
+  const regex = /^(GET|POST|PUT|PATCH|DELETE|HEAD|OPTIONS)\b/;
+  const method = lineUntilCursor.match(regex)?.[1]?.toLowerCase();
+
+  let rawItems: string[] = [];
+  if (method && supportedMethods.includes(method)) {
+    rawItems = getCompletionForEndpoints(currentLine, lineUntilCursor, method);
+  } else {
+    rawItems = getCompletionForAttributes(currentLine, lineUntilCursor);
+  }
+
+  const items = rawItems.slice(0, MAX_ITEMS).map((x) => ({ label: x }));
+
+  return {
+    isIncomplete: rawItems.length > MAX_ITEMS,
+    items,
+  };
+};
+
+const getCompletionForAttributes = (
+  curentLine: string,
+  lineUntilCursor: string,
+) => {
   const m = lineUntilCursor.match(/([:\[])?(\s*)(\w+)$/);
   const delimiter = m?.[1] ?? ""; // ":" or "[" if present
   const whitespace = m?.[2] ?? ""; // any spaces after delimiter
@@ -68,35 +90,39 @@ export const completion = (message: RequestMessage): CompletionList | null => {
     prefix = whitespace === "" ? `${delimiter}${word}` : word;
   }
 
-  const regex = /^(GET|POST|PUT|PATCH|DELETE|HEAD|OPTIONS)\b/;
-  const method = lineUntilCursor.match(regex)?.[1]?.toLowerCase();
-
-  let rawItems: string[] = [];
-  if (method && supportedMethods.includes(method)) {
-    rawItems = getEndpoints()
-      .flatMap((endpoint) => endpoint[method as Method])
-      .map((endpoint) => endpoint.route);
-  } else {
-    rawItems = ITEMS;
-  }
-
-  const items = rawItems
-    .filter((x) => x.toLocaleLowerCase().startsWith(prefix.toLocaleLowerCase()))
-    .slice(0, MAX_ITEMS)
-    .map((x) => ({ label: x }));
+  const completionItems = ITEMS.filter((x) =>
+    x.toLocaleLowerCase().startsWith(prefix.toLocaleLowerCase()),
+  );
 
   log.writeIndented({
     prefixFINAL: prefix,
     word,
     delimiter,
     whitespace,
-    items,
+    completionItems,
   });
 
-  return {
-    isIncomplete: rawItems.length > MAX_ITEMS,
-    items,
-  };
+  return completionItems;
+};
+
+const endpointRegex =
+  /^(GET|POST|PUT|PATCH|DELETE|HEAD|OPTIONS)\b\s*([a-zA-Z/\-_]*)/;
+const getCompletionForEndpoints = (
+  curentLine: string,
+  lineUntilCursor: string,
+  method: string,
+) => {
+  const m = lineUntilCursor.match(endpointRegex);
+  const partialEndpointCapture = m?.[2] ?? "";
+
+  const partialEndpoint = partialEndpointCapture.startsWith("/")
+    ? partialEndpointCapture
+    : `/${partialEndpointCapture}`;
+
+  return getEndpoints()
+    .flatMap((endpoint) => endpoint[method as Method])
+    .filter((endpoint) => endpoint.route.startsWith(partialEndpoint))
+    .map((endpoint) => endpoint.route);
 };
 
 const ITEMS = [
